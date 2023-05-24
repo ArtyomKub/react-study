f0584a9e2166b74d4c99b9762c67feb7a49f4079
ArtyomKub <coder.kub.lover@gmail.com>
2023-05-24 22:04:33 +0200

    Create new component 'Star' with function

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ function App() {
 function Rating() {
     return (
         <div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
     )
 }
@@ -33,6 +33,12 @@ function Accordion() {
                 <li>3</li>
             </ul>
         </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>Star</div>
     )
 }
 
